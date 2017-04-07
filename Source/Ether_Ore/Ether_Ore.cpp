455c902243a721c8d34e82bbe9975398a40398fb
rifffyreblade <hollisandrew@hotmail.com>
2017-04-06 21:22:42 -0400

    Added C++

---
+++ Source/Ether_Ore/Ether_Ore.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Ether_Ore.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ether_Ore, "Ether_Ore" );
---
+++ Source/Ether_Ore/Public/PlayerControllerWrapper.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "PlayerControllerWrapper.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ETHER_ORE_API APlayerControllerWrapper : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
